758739c4a0688a3f5dfd68367757e047ab8f2212
Andreas Pfeiffer <andreas.pfeiffer@cern.ch>
2014-01-14 15:18:40 +0100

    cleaned up TopDAS branch

--- MuScleFit/Calibration/python/musclefitmuoncorrector_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-MuScleFit = cms.EDProducer("MuScleFitPATMuonCorrector", 
-                         src = cms.InputTag("patMuonsWithTrigger"), 
-                         debug = cms.bool(True), 
-                         identifier = cms.string("Summer12_DR53X"), 
-                         applySmearing = cms.bool(True), 
-                         fakeSmearing = cms.bool(False)
-                         )
